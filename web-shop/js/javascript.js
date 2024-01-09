2aaace759b4bc3afef8d5ac60756ba94da195ac3
isakMurtezani <isakmurtezani@gmail.com>
2024-01-09 14:50:06 +0100

    update

---
+++ web-shop/js/javascript.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function adjustIframeHeight(iframe) {
+        iframe.onload = function () {
+            var frameContent = iframe.contentDocument || iframe.contentWindow.document;
+            var object = frameContent.querySelector("object");
+            var frameBody = frameContent.querySelector("body");
+            var height = object.offsetHeight;
+            frameBody.style.margin = 0;
+            iframe.style.height = height + 'px';
+        };
+    }
+    var iframes = document.querySelectorAll('.auto-height-iframe');
+    iframes.forEach(adjustIframeHeight);
+});

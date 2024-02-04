ab41113b60171881bd6728dc9310f989a55f9eab
isakMurtezani <isakmurtezani@gmail.com>
2024-02-04 14:24:17 +0100

    update

---
+++ web-shop/js/message.js
@@ -0,0 +1,19 @@
+let text = ""
+        const nasText = text.split("")
+        
+        for (i = 0; i < nasText.length; i++) {
+            nasText[i];
+        }
+           
+        function splice(){
+            
+            let zadnji = nasText.pop();
+            nasText.unshift(zadnji);
+            let miao = nasText.join("");
+            
+            
+            document.getElementById("rotating-message").innerHTML=miao;
+            console.log(miao);
+        }
+        setInterval(splice, 300);
+        
